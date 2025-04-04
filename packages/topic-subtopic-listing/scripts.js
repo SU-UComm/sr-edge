43c7f76701b9fb4672f3bee5583e80e705043144
Rafał Szarafiński <rszarafinski@squiz.pl>
2025-04-04 16:26:48 +0200

    Fixing cards html that was fetched

--- packages/topic-subtopic-listing/scripts.js
+++ packages/topic-subtopic-listing/scripts.js
@@ -191,7 +191,7 @@ export async function handleButtonClick(args) {
         `
     }).join('')
 
-    updateState(section, cards, pager, modals);
+    updateState(section, cards.join(''), pager, modals);
 }
 
 /**
