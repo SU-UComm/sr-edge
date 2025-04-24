b7eefdc6a7e2fdc2b78103af635755fb98aff02c
Rafał Szarafiński <rszarafinski@squiz.pl>
2025-04-24 07:41:45 +0200

    Peer review fixes

--- global/js/utils/basicHeroDataAdapter/basicHeroDataAdapter.js
+++ global/js/utils/basicHeroDataAdapter/basicHeroDataAdapter.js
@@ -1,13 +1,24 @@
 /**
- * Orchestrates the transformation of data from
- * various endpoints
+ * Adapter class to orchestrate the transformation of data
+ * from various endpoints, using a provided basic hero service.
  */
 export class basicHeroDataAdapter {
+    /**
+     * Creates an instance of basicHeroDataAdapter.
+     *
+     * @param {Object} basicHeroService - A service object that provides the `getBasicHeroData` method.
+     */
     constructor(basicHeroService) {
         this.basicHeroService = basicHeroService;
         this.getBasicHeroData = basicHeroService?.getBasicHeroData;
     }
 
+    /**
+     * Sets or replaces the hero service and rebinds its `getBasicHeroData` method.
+     *
+     * @param {Object} basicHeroService - The new basic hero service instance.
+     * @param {Function} basicHeroService.getBasicHeroData - Method to fetch hero data by asset ID.
+     */
     setBasicHeroService(basicHeroService) {
         this.basicHeroService = basicHeroService;
         this.getBasicHeroData = basicHeroService.getBasicHeroData.bind(basicHeroService);
--- global/js/utils/matrixBasicHeroService/matrixBasicHeroService.js
+++ global/js/utils/matrixBasicHeroService/matrixBasicHeroService.js
@@ -1,25 +1,36 @@
-
+/**
+ * Service class for retrieving basic hero data from the Matrix API.
+ */
 export class matrixBasicHeroService {
+    /**
+     * Constructs the service with a base domain URL.
+     * 
+     * @param {Object} config - Configuration object.
+     * @param {string} config.BASE_DOMAIN - The base domain for the API.
+     */
     constructor({ BASE_DOMAIN }) {
         this.BASE_DOMAIN = BASE_DOMAIN;
     }
 
+    /**
+     * Fetches basic hero data for a given asset ID from the Matrix API.
+     *
+     * @async
+     * @param {string} currentAssetId - The asset ID to fetch hero data for.
+     * @returns {Promise<Object>} A promise that resolves to the hero data.
+     * @throws {Error} If the fetch request or JSON parsing fails.
+     */
     async getBasicHeroData(currentAssetId) {
         const query = `${this.BASE_DOMAIN}_api/mx/storyhero?story=${currentAssetId}`;
 
-        const res = await fetch(query).catch((error) => {
+        try {
+            const res = await fetch(query);
+            const json = await res.json();
+
+            return json;
+        } catch (error) {
             throw new Error(error);
-        });
-
-        const json = await res.json();
-
-        return json;
-
-
-        // return Promise.all(json)
-        //     .catch((error) => {
-        //         throw new Error(error);
-        //     });
+        }
     }
 }
 
